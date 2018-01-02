8b92fe2e7e5d14c3d62b2497fd54b5ba21e08d67
agigryms <agnieszka.grzymkowska@gmail.com>
2018-01-02 09:49:21 +0100

    Add script that sets env variable PATH for Cron job; add run python file with console output to the custom log to track sending emails and number of new alerts.

---
+++ src/run_alert_updater.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#echo $PATH
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+#echo $PATH
+
+/home/agigryms/miniconda3/bin/python /home/agigryms/Notebooks/Flat_Ads/Flat_Ads/src/run_w_console_output_time.py
